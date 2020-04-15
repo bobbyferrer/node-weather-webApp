16b469d91d9e9a82ee10e3d6c0b0ebfcc88ecc21
iambobbyferrer <mtditrainer.bobbyferrer@gmail.com>
2020-04-16 00:43:49 +0800

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
 	msg.textContent = 'Loading..';
 	forecastMsg.textContent = '';
 
-	fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+	fetch(`/weather?address=${location}`).then((response) => {
 		response.json().then((data) => {
 			if (data.error) {
 				msg.textContent = data.error;
